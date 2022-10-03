d92e918140192beda46d7522e3e730e5f4cad5fa
Frederic <zetachow@163.com>
2022-10-04 00:30:56 +0800

    test ipld

--- go.mod
+++ go.mod
@@ -10,22 +10,37 @@ require (
 	github.com/gen2brain/beeep v0.0.0-20220909211152-5a9ec94374f6 // indirect
 	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/ipfs/go-cid v0.2.0 // indirect
+	github.com/ipld/go-ipld-prime v0.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/mr-tron/base58 v1.1.0 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/multiformats/go-base32 v0.0.3 // indirect
 	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multihash v0.2.0 // indirect
+	github.com/multiformats/go-varint v0.0.6 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/polydawn/refmt v0.0.0-20201211092308-30ac6d18308e // indirect
 	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	lukechampine.com/blake3 v1.1.6 // indirect
 )
 
 require (
+	github.com/frankban/quicktest v1.14.3
 	github.com/gdamore/tcell v1.4.0
 	github.com/multiformats/go-multibase v0.1.1
 	github.com/rivo/tview v0.0.0-20220916081518-2e69b7385a37
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/sys v0.0.0-20220926163933-8cfa568d3c25 // indirect
 )
---
+++ test/ipld_test.go
@@ -0,0 +1,117 @@
+package test
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/codec/dagjson"
+	"github.com/ipld/go-ipld-prime/must"
+	"github.com/ipld/go-ipld-prime/node/basicnode"
+	"github.com/ipld/go-ipld-prime/node/bindnode"
+	"github.com/ipld/go-ipld-prime/schema"
+)
+
+func TestCreateData(t *testing.T) {
+	np := basicnode.Prototype.Any // Pick a prototype: this is how we decide what implementation will store the in-memory data.
+	nb := np.NewBuilder()         // Create a builder.
+	ma, _ := nb.BeginMap(2)       // Begin assembling a map.
+	ma.AssembleKey().AssignString("hey")
+	ma.AssembleValue().AssignString("it works!")
+	ma.AssembleKey().AssignString("yes")
+	ma.AssembleValue().AssignBool(true)
+	ma.Finish()     // Call 'Finish' on the map assembly to let it know no more data is coming.
+	n := nb.Build() // Call 'Build' to get the resulting Node.  (It's immutable!)
+
+	buf := []byte{}
+	b := bytes.NewBuffer(buf)
+
+	dagjson.Encode(n, b)
+
+	fmt.Println(b.String())
+
+}
+
+func TestStructWithSchema(t *testing.T) {
+	type Person struct {
+		Name    string
+		Age     int
+		Friends []string
+	}
+
+	ts, err := ipld.LoadSchemaBytes([]byte(`
+			type Person struct {
+				name    String
+				age     Int
+				friends [String]
+			} representation tuple
+		`))
+	if err != nil {
+		panic(err)
+	}
+	schemaType := ts.TypeByName("Person")
+	person := &Person{Name: "Alice", Age: 34, Friends: []string{"Bob"}}
+	node := bindnode.Wrap(person, schemaType)
+
+	fmt.Printf("%#v\n", person)
+
+	buf := []byte{}
+	b := bytes.NewBuffer(buf)
+
+	dagjson.Encode(node.Representation(), b)
+
+	fmt.Println(b.String())
+
+}
+
+func TestMarshel(t *testing.T) {
+	type Foobar struct {
+		Foo string
+		Bar string
+	}
+	encoded, err := ipld.Marshal(dagjson.Encode, &Foobar{"wow", "whee"}, nil)
+	fmt.Printf("error: %v\n", err)
+	fmt.Printf("data: %s\n", string(encoded))
+
+}
+
+func TestUnmarshel(t *testing.T) {
+	serial := strings.NewReader(`{"hey":"it works!","yes": true}`)
+
+	np := basicnode.Prototype.Any // Pick a stle for the in-memory data.
+	nb := np.NewBuilder()         // Create a builder.
+	dagjson.Decode(nb, serial)    // Hand the builder to decoding -- decoding will fill it in!
+	n := nb.Build()               // Call 'Build' to get the resulting Node.  (It's immutable!)
+
+	fmt.Printf("the data decoded was a %s kind\n", n.Kind())
+	fmt.Printf("the length of the node is %d\n", n.Length())
+
+}
+
+func TestUnmarshelWithSchema(t *testing.T) {
+	typesys := schema.MustTypeSystem(
+		schema.SpawnStruct("Foobar",
+			[]schema.StructField{
+				schema.SpawnStructField("foo", "String", false, false),
+				schema.SpawnStructField("bar", "String", false, false),
+			},
+			schema.SpawnStructRepresentationMap(nil),
+		),
+		schema.SpawnString("String"),
+	)
+
+	type Foobar struct {
+		Foo string
+		Bar string
+	}
+	serial := []byte(`{"foo":"wow","bar":"whee"}`)
+	foobar := Foobar{}
+	n, err := ipld.Unmarshal(serial, dagjson.Decode, &foobar, typesys.TypeByName("Foobar"))
+	fmt.Printf("error: %v\n", err)
+	fmt.Printf("go struct: %v\n", foobar)
+	fmt.Printf("node kind and length: %s, %d\n", n.Kind(), n.Length())
+	fmt.Printf("node lookup 'foo': %q\n", must.String(must.Node(n.LookupByString("foo"))))
+
+}
